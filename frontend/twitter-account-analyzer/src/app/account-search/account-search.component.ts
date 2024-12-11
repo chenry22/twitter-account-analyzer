f530d68ce1a58ddf5f25008489261645a7955571
chenry22 <112424609+chenry22@users.noreply.github.com>
2024-12-10 23:58:16 -0600

    add basic search bar (not functional)

--- frontend/twitter-account-analyzer/src/app/account-search/account-search.component.ts
+++ frontend/twitter-account-analyzer/src/app/account-search/account-search.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-account-search',
-  imports: [],
+  imports: [MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './account-search.component.html',
   styleUrl: './account-search.component.css'
 })
